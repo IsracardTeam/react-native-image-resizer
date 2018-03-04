e7a80a37f9b214895d78542415d84c8343b2fd30
IsracardTeam <33055665+IsracardTeam@users.noreply.github.com>
2018-03-04 19:22:36 +0200

    Update index.android.js

--- index.android.js
+++ index.android.js
@@ -1,7 +1,6 @@
 import React from 'react-native';
 
 const ImageResizerAndroid = React.NativeModules.ImageResizerAndroid;
-
 export default {
   createResizedImage: (imagePath, newWidth, newHeight, compressFormat, quality, rotation = 0, outputPath) => {
     return new Promise((resolve, reject) => {
@@ -9,4 +8,10 @@ export default {
         compressFormat, quality, rotation, outputPath, resolve, reject);
     });
   },
+  resizeImageToCertainSize: (imagePath, targetSizeInBytes, newWidth, compressFormat, quality, rotation = 0, outputPath) => {
+    return new Promise((resolve, reject) => {
+      ImageResizerAndroid.resizeImageToCertainSize(imagePath, targetSizeInBytes,newWidth,
+        compressFormat, quality, rotation, outputPath, resolve, reject);
+    });
+  }
 };
