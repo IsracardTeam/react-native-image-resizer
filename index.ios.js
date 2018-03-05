f7974d733fc2a0ea3d07692df7d15ea441205318
IsracardTeam <33055665+IsracardTeam@users.noreply.github.com>
2018-03-05 13:04:07 +0200

    Update index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -18,4 +18,20 @@ export default {
       });
     });
   },
+
+  resizeImageToCertainSize: (path, targetSizeInBytes, format, quality, rotation = 0, outputPath) => {
+    if (format !== 'JPEG' && format !== 'PNG') {
+      throw new Error('Only JPEG and PNG format are supported by createResizedImage');
+    }
+
+    return new Promise((resolve, reject) => {
+      NativeModules.ImageResizer.resizeImageToCertainSize(path, targetSizeInBytes, format, quality, rotation, outputPath, (err, response) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve(response);
+      });
+    });
+  }
 };
